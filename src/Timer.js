21e280ae2e34118717762c781cb310f8f5d9c9b7
TiffD <tiffany.dessouroux@gmail.com>
2021-11-22 12:30:39 +0100

    start/pause buton functionnal

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,5 @@
 import React,{useState, useEffect} from 'react';
 import BreakTimer from './BreakTimer';
-import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
 
 
 
@@ -9,18 +7,20 @@ export function Timer (){
     const [minutesLeft, setMinutesLeft]= useState(25);
     const [seconds, setSeconds]= useState(0);
     const [displayTimer, setDisplayTimer]=useState(false);
-    const [mode, switchMode]=useState('work');
-<CircularProgressbar value={40} text='40' styles={buildStyles({
-pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
-textColor: '#f88',
-trailColor: '#d6d6d6',
-backgroundColor: '#3e98c7',
-})}/>
+    const [isActive, setIsActive]=useState(false);
+
 
 
 useEffect(()=>{
     const startTimer = setInterval(()=>{
 clearInterval(startTimer)
+
+let intervalID;
+
+if(isActive){
+    intervalID=setInterval(()=>{
+        
+    })
 
         if(seconds=== 0){
             if(minutesLeft!== 0){
@@ -40,10 +40,11 @@ clearInterval(startTimer)
             setSeconds(seconds -1)
             
         }
-
+    }
 
     },1000);
-}, [seconds]);
+}, [isActive, seconds]);
+
 
 
 
@@ -53,6 +54,11 @@ clearInterval(startTimer)
         <div className="timer-comp">
             <h2>Time to work !</h2>
       {minutesLeft}:{seconds}
+        </div>
+        <div className="start__stop__buttons">
+            <button onClick={()=>setIsActive(!isActive)}>{isActive? 'Pause' : 'Start'}</button>
+            <button>Stop</button>
+            
         </div>
         <div className="plus__button">
     <button className="plus" onClick={()=>setMinutesLeft(minutesLeft +1)}>+</button>
